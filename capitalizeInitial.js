bec6a961c7afb798ea7d3f2aee48b374f39521bb
ben <beatuscanis@hotmail.com>
2020-12-15 08:41:49 +0100

    clean up in capitalizedInitial

--- capitalizeInitial.js
+++ capitalizeInitial.js
@@ -1,10 +1,12 @@
 let capitalizeInitial = (str) => {
-    return str.split(' ')
+    let capiltalized = str
+        .split(' ')
         .map(item =>
             item[0].toUpperCase()
             +
             item.slice(1))
         .join(' ');
+    return capiltalized
 }
 
 module.exports = { capitalizeInitial }
